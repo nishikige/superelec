5f446a5c597de35120cffac505d5a21ccfb6a712
nishikige <42238591+nishikige@users.noreply.github.com>
2022-02-21 23:57:04 +1100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,8 +21,10 @@ class Error404 extends React.Component {
             <div className="justify-items-center align-middle flex-grow max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 text-white">
                 <div className="inset-0">
                     Just a bit of bantz. Unlike (UNSW) <a href="https://www.elsoc.net/">ELSOC</a>, we have a website built from scratch and not requiring some third party website platform.
-                    <img className="self-center justify-center w-3/4 h-3/4 sm:w-1/3 sm:h-1/3 lg:w-1/4 lg:h-1/4" src={pikachu} alt="Suprise Pikachu" />
                 </div>
+            </div>
+            <div className = "flex flex-box flex-col text-center">
+                    <img className="self-center justify-center w-3/4 h-3/4 sm:w-1/3 sm:h-1/3 lg:w-1/4 lg:h-1/4" src={pikachu} alt="Suprise Pikachu" />
             </div>
             </>
         );
