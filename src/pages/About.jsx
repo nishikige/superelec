cfaeb905867b7f0ea15e658138496fbd5b5c1091
nishikige <42238591+nishikige@users.noreply.github.com>
2022-02-21 23:40:50 +1100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,16 +8,14 @@ class Error404 extends React.Component {
     render() {
         return(
             <>
-            /*
             <Helmet>
                     <meta charSet="utf-8" />
                     <title>About - Super Elec</title>
-                    <meta property="og:title" content="Error404 - Super Elec"/>
+                    <meta property="og:title" content="About - Super Elec"/>
                     <meta property="og:image" content={`${import.meta.env.VITE_BASE_URL}${super_elec_logo_png}`}/>
-                    <meta property="og:description" content="Page Not Found"/>
+                    <meta property="og:description" content="About Us Super Elec Peeps"/>
                     <meta property="og:url" content={`${import.meta.env.VITE_BASE_URL}`}/>
             </Helmet>
-            */
             <div className="justify-items-center align-middle flex-grow max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 text-white">
                 <div className="inset-0">
                     Just a bit of bantz. Unlike (UNSW) ELSOC, we have a website built from scratch and not requiring some third party website platform.
