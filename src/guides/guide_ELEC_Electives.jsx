3d329aaef6a318489925c9a33e7aad357754c08f
nishikige <42238591+nishikige@users.noreply.github.com>
2022-02-24 13:21:47 +1100

    Create guide_ELEC_Electives.jsx

---
+++ src/guides/guide_ELEC_Electives.jsx
@@ -0,0 +1,4 @@
+import guide_ELEC_Electives from './guide_ELEC_Electives'
+
+<iframe src="https://drive.google.com/file/d/1_0siUFsxsZpPaDiBHW2uOTQjbGrXG-Cp/preview" width="640" height="480" allow="autoplay"></iframe>
+
